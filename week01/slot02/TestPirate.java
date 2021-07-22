5ffc4606f40e8fc5c6b377aa9b3c1fd17f3b0789
Jailani Abdul Rahman <jailani.har@googlemail.com>
2021-07-22 15:42:24 +0800

    Sourcecode for Week 01 Slot 02

--- week01/slot02/TestPirate.java
+++ week01/slot02/TestPirate.java
@@ -2,4 +2,29 @@ package week01.slot02;
 
 public class TestPirate {
 
+	public static void main(String[] args) {
+		Pirate pirate1 = new Pirate("Bakar", -10);
+		System.out.println(pirate1);
+		
+		Pirate pirate2 = new Pirate("Curi", 5);
+		System.out.println(pirate2);
+		Pirate pirate3 = new Pirate("Daging", 8);
+		System.out.println(pirate3);
+		
+		pirate2.health--;
+		
+		System.out.println(pirate1);
+		System.out.println(pirate2);
+		System.out.println(pirate3);
+		
+//		System.out.println(pirate1.name);
+//		System.out.println(pirate1.health);
+		
+//		pirate1.name = "Abu";
+//		pirate1.health = -5;
+		
+//		System.out.println(pirate1.name);
+//		System.out.println(pirate1.health);
+	}
+	
 }
